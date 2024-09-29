36ac9a2e0fc6f4c6539e27a4f8efc081aebebba3
kim-minkyoung <decomin02@pusan.ac.kr>
2024-09-29 15:13:30 +0900

    initial commit

---
+++ src/test/java/com/example/rl_simulation_wep/RlSimulationWepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rl_simulation_wep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlSimulationWepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
